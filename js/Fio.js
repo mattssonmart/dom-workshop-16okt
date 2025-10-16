f5e6e50e9649b3cf1c493310f10d5281d2c0f9b7
FiorellaDiGiulio <fiorella.digiulio@chasacademy.se>
2025-10-16 11:47:31 +0200

    uppgift1del2

---
+++ js/Fio.js
@@ -0,0 +1,9 @@
+// driver : Fiorella
+// nav    : Ahm, Oligerta, Mårten
+
+const btn1 = document.querySelector ("#btn1");
+
+btn1.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#f7cb15"
+    document.body.style.color = "#f55d3e"
+});
