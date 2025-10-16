58e28b2f6db4ff80798d7a597ba991872b3746e6
ahmringholm <ringholmahm4@gmail.com>
2025-10-16 11:20:47 +0200

    ahm

---
+++ js/Ahm.js
@@ -0,0 +1,7 @@
+// Driver: Ahm
+// Navigator: Oligerta, Fiorella, Mårten
+
+const button = document.createElement("bakgrund");
+bakgrund.textContent = "Byt bakgrundsfärg";
+bakgrund.style.bakgrund = "#c8f0a8";
+document.getElementById("bakgrund").appendChild("button")
